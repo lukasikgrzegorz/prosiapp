20a2472724c55a0e1036dace6eaa9478ab2039b2
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-22 23:50:18 +0100

    Style new item form

--- src/components/NewItemForm/NewItemForm.jsx
+++ src/components/NewItemForm/NewItemForm.jsx
@@ -2,57 +2,102 @@ import { addDoc, collection } from "firebase/firestore";
 import { useEffect, useRef, useState } from "react";
 import { db } from "../../services/firebase";
 import getActualDate from "../../functions/getActualDate";
+import { getUserID } from "../../redux/selectors";
+import { useSelector } from "react-redux";
+import css from "./NewItemForm.module.css";
+import Button from "../../components/Button/Button";
 
 const NewItemForm = () => {
-	const [title, setTitle] = useState();
-	const [value, setValue] = useState();
+	const [title, setTitle] = useState(null);
+	const [value, setValue] = useState(null);
 	const [date, setDate] = useState();
 	const [isIncome, setIsIncome] = useState(true);
-	const inputDataRef = useRef();
+	const inputDateRef = useRef();
+	const inputTitleRef = useRef();
+	const inputValueRef = useRef();
 	const actualDate = getActualDate();
+	const userID = useSelector(getUserID);
 
 	useEffect(() => {
 		setDate(actualDate);
 	}, []);
 
+	const clearInput = () => {
+		inputTitleRef.current.value = "";
+		inputValueRef.current.value = "";
+		setValue(null);
+		setTitle(null);
+	};
+
 	const setIncome = () => {
 		setIsIncome(true);
+		clearInput();
 	};
 
 	const setCost = () => {
 		setIsIncome(false);
+		clearInput();
+	};
+
+	const setActualValue = (e) => {
+		if (isIncome) {
+			setValue(e.target.value * 1);
+		} else {
+			setValue(e.target.value * -1);
+		}
 	};
 
 	const addNewItem = async () => {
-		try {
-			const docRef = await addDoc(collection(db, `b2MXBNKz38Ml3p5KnHwOkvLJgPw1`), {
-				value: value,
-				title: title,
-				date: date,
-			});
-			console.log("Document written with ID: ", docRef.id);
-		} catch (e) {
-			console.error("Error adding document: ", e);
+		if (value && title) {
+			try {
+				const docRef = await addDoc(collection(db, userID), {
+					value: value,
+					title: title,
+					date: date,
+				});
+				console.log("Document written with ID: ", docRef.id);
+			} catch (e) {
+				console.error("Error adding document: ", e);
+			} finally {
+				clearInput();
+			}
+		} else {
+			console.log("Puste pola!");
 		}
 	};
 
 	return (
-		<>
-			<div>
-				<button onClick={setIncome}>INCOME</button>
-				<button onClick={setCost}>COST</button>
+		<div className={isIncome ? css["wrapper-green"] : css["wrapper-red"]}>
+			<div className={css["button-wrapper"]}>
+				<button onClick={setIncome} className={css["accent"]} disabled={isIncome}>
+					+ Income
+				</button>
+				<button onClick={setCost} className={css["accent"]} disabled={!isIncome}>
+					- Cost
+				</button>
 			</div>
-			<input type="text" placeholder="title" onChange={(e) => setTitle(e.target.value)} />
-			<input type="number" placeholder="value" onChange={(e) => setValue(e.target.value)} />
-			<input
-				ref={inputDataRef}
-				type="date"
-				placeholder="date"
-				defaultValue={actualDate}
-				onChange={(e) => setDate(e.target.value)}
-			/>
-			<button onClick={addNewItem}>Add</button>
-		</>
+			<div className={css["input-wrapper"]}>
+				<input
+					ref={inputTitleRef}
+					type="text"
+					placeholder="Title"
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<div className={css["input-row"]}>
+					<input ref={inputValueRef} type="number" placeholder="Value" onChange={setActualValue} />
+					<input
+						ref={inputDateRef}
+						type="date"
+						placeholder="date"
+						defaultValue={actualDate}
+						onChange={(e) => setDate(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div>
+				<Button value="Add" onClickHandler={addNewItem} />
+			</div>
+		</div>
 	);
 };
 
